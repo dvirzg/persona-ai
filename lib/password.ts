0e5a1317b77f3c6d876e5b7e12e8b7ebeb632400
Dvir Zagury Grynbaum <dvirzagury@gmail.com>
2024-12-29 16:39:55 -0500

    replace pass hashing method: argon2 -> bcrypt

--- lib/password.ts
+++ lib/password.ts
@@ -1,12 +1,12 @@
-import { hash, verify } from '@node-rs/argon2';
+import { hash, compare } from 'bcrypt-ts';
 
 export async function hashPassword(password: string): Promise<string> {
-  return hash(password);
+  return hash(password, 10);
 }
 
 export async function verifyPassword(
   password: string,
   hashedPassword: string,
 ): Promise<boolean> {
-  return verify(hashedPassword, password);
+  return compare(password, hashedPassword);
 } 
